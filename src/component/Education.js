c91e54e7be37f2b393319e0278d75a50d5c7a76e
waqas saleem <waqas.mba86@gmail.com>
2020-03-11 08:51:19 -0700

    adding of more pages

--- src/component/Education.js
+++ src/component/Education.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import '../App.css'
-
+import {Link} from 'react-router-dom'
 
 
 class Education extends Component {
@@ -16,7 +16,33 @@ constructor(){
     render() {        
         return (
             <div>
-               Education page
+               <h3 style={{textAlign:'center'}}>Education </h3>
+
+               <div class="row">
+                    
+                    {/* // Grid Coloumn-1/2 */}
+                    <div class="col s2"> 
+                    <Link to='/mba'>Marks Detail- MBA</Link> <br/>
+                    <Link to='/bcom'>Marks Detail- B.com</Link> <br/>
+                    <Link to='/fa'>Marks Detail- Intermediate</Link> <br/>
+                    <Link to='/matric'>Marks Detail- Matriculation</Link> <br/>
+                    </div>
+                    
+                    
+                     {/* //Grid Coloumn-2/2 */}
+                    <div class="col s10">  
+                    
+                    <div class="collection">
+                    <a href="#!" class="collection-item">Alvin</a>
+                    <a href="#!" class="collection-item">Alvin</a>
+                    
+                     </div>
+                    
+                     </div>
+                    
+            </div>
+
+
             </div>
 
         )
--- src/component/Personal.js
+++ src/component/Personal.js
@@ -17,21 +17,27 @@ constructor(){
         return (
             <div>
 
-<ul class="collection center container">
-      <li className="collection-item center"><p> <b>Name</b> <br/>WAQAS SALEEM</p></li>
-      <li className="collection-item center"><p> <b>Father Name</b> <br/>MUHAMMAD SALEEM</p></li>
-      <li className="collection-item center"><p> <b>Nationality</b> <br/>Pakistan</p></li>
-      <li className="collection-item center"><p> <b>C.N.I.C Number</b> <br/>31303-6611734-5</p></li>
-      <li className="collection-item center"><p> <b>Domicile</b> <br/>Punjab, Faisalabad</p></li>
-      <li className="collection-item center"><p> <b>E-mail</b> <br/>waqas.mba86@gmail.com <br/>waqas_mba86@yahoo.com</p></li>
-      <li className="collection-item center"><p> <b>Contact Number</b> <br/>+923467605798<br/>+923167558180</p></li>
-      <li className="collection-item center"><p> <b>Address</b> <br/>Street No.7, P-4671/6, Near Qadri Marriage Hall, 66-foota bazar, Mansoorabad, Faisalabad, Pakistan.</p></li>
-</ul>
+                    <div class="row">
+                    
+                    
 
+<ul class="collection container">
+      <li className="collection-item "><p> <b>Name</b> <br/>WAQAS SALEEM</p></li>
+      <li className="collection-item "><p> <b>Father Name</b> <br/>MUHAMMAD SALEEM</p></li>
+      <li className="collection-item "><p> <b>Nationality</b> <br/>Pakistan</p></li>
+      <li className="collection-item "><p> <b>C.N.I.C Number</b> <br/>31303-6611734-5</p></li>
+      
+
+      <li className="collection-item "><p> <b>Domicile</b> <br/>Punjab, Faisalabad</p></li>
+      <li className="collection-item "><p> <b>E-mail</b> <br/>waqas.mba86@gmail.com <br/>waqas_mba86@yahoo.com</p></li>
+      <li className="collection-item "><p> <b>Contact Number</b> <br/>+923467605798<br/>+923167558180</p></li>
+      <li className="collection-item "><p> <b>Address</b> <br/>Street No.7, P-4671/6, Near Qadri Marriage Hall, 66-foota bazar, Mansoorabad, Faisalabad, Pakistan.</p></li>
+</ul>
+</div>
 
   <div className='center'><Link to='/Education'><button className="waves-effect waves-light btn-large">My Education Info</button></Link></div>
+            
             </div>
-
         )
 
 
